5213e6cf8fed563faaacf974806041271640b6ee
ddenaga <76412992+ddenaga@users.noreply.github.com>
2022-01-15 19:13:42 -0800

    Implemented adapter class for the recyclerView

--- app/src/main/java/com/github/ddenaga/simpletodo/MainActivity.kt
+++ app/src/main/java/com/github/ddenaga/simpletodo/MainActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val tasks: MutableList<String> = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +19,10 @@ class MainActivity : AppCompatActivity() {
         val btnAdd = findViewById<Button>(R.id.btnAdd)
         val etTask = findViewById<EditText>(R.id.etTask)
         val rvTasks = findViewById<RecyclerView>(R.id.rvTasks)
+
+        // Binding the adapter to the recyclerView.
+        val adapter = TasksAdapter(tasks)
+        rvTasks.adapter = adapter
+        rvTasks.layoutManager = LinearLayoutManager(this)
     }
 }

fd7da05b72cc6b93b34dbaad9f3c73a7a60992b8
ddenaga <76412992+ddenaga@users.noreply.github.com>
2022-01-15 18:44:14 -0800

    Added views

--- app/src/main/java/com/github/ddenaga/simpletodo/MainActivity.kt
+++ app/src/main/java/com/github/ddenaga/simpletodo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.github.ddenaga.simpletodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Reference views from the main layout.
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val etTask = findViewById<EditText>(R.id.etTask)
+        val rvTasks = findViewById<RecyclerView>(R.id.rvTasks)
     }
 }
